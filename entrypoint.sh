d0c75ab30cab7f9f77b260d085b640393b57f5dd
Alexander Trost <galexrt@googlemail.com>
2020-01-31 17:35:11 +0100

    use RSYNC_ARGS env var for entrypoint
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
 # Do deployment
-sh -c "rsync -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $@"
+sh -c "rsync -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $RSYNC_ARGS"
